8aad86705c2f0206179bf927a7ca6b923ad5b37b
hexahigh <97455552+hexahigh@users.noreply.github.com>
2023-09-12 18:28:26 +0200

    kuk

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,28 @@
 <script>
     import Countdown from "./countdown.svelte";
+    import { MetaTags } from "svelte-meta-tags";
 </script>
 
-<svelte:head>
+<MetaTags
+    title="Kukfest"
+    titleTemplate="%s | Kukfest"
+    description="Kukfest har masse kuk"
+    canonical="https://kukfest.eu"
+    openGraph={{
+        url: "https://www.url.ie/a",
+        title: "Kukfest",
+        description: "Mye kuk",
+        images: [
+            {
+                url: "https://www.example.ie/og-image-01.jpg",
+                width: 800,
+                height: 600,
+                alt: "Og Image Alt",
+            },],
+        siteName: "Kukfest",
+    }}
+/>
 
-</svelte:head>
+<svelte:head />
 
-<Countdown/>
+<Countdown />
