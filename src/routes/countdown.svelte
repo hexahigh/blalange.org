f1e18bc19ea3ce6ed1e869a2351ca702c6c7f925
hexahigh <97455552+hexahigh@users.noreply.github.com>
2023-09-12 18:22:54 +0200

    added a countdown

---
+++ src/routes/countdown.svelte
@@ -0,0 +1,137 @@
+<script>
+    (function () {
+        const second = 1000,
+            minute = second * 60,
+            hour = minute * 60,
+            day = hour * 24;
+
+            let today = new Date();
+            let yyyy = today.getFullYear();
+            let monthDay = "10/16/";
+            let date = monthDay + yyyy;
+
+        const countDown = new Date(date).getTime(),
+            x = setInterval(function () {
+                const now = new Date().getTime(),
+                    distance = countDown - now;
+
+                (document.getElementById("days").innerText = Math.floor(
+                    distance / day
+                )),
+                    (document.getElementById("hours").innerText = Math.floor(
+                        (distance % day) / hour
+                    )),
+                    (document.getElementById("minutes").innerText = Math.floor(
+                        (distance % hour) / minute
+                    )),
+                    (document.getElementById("seconds").innerText = Math.floor(
+                        (distance % minute) / second
+                    ));
+
+                //do something later when date is reached
+                if (distance < 0) {
+                    document.getElementById("headline").innerText =
+                        "Kukfest is currently happening!";
+                    document.getElementById("countdown").style.display = "none";
+                    document.getElementById("content").style.display = "block";
+                    clearInterval(x);
+                }
+                //seconds
+            }, 0);
+    })();
+</script>
+
+<svelte:head />
+
+<div class="container">
+    <h1 id="headline">Countdown to Kukfest 23.1</h1>
+    <div id="countdown">
+        <ul>
+            <li><span id="days" />days</li>
+            <li><span id="hours" />Hours</li>
+            <li><span id="minutes" />Minutes</li>
+            <li><span id="seconds" />Seconds</li>
+        </ul>
+    </div>
+    <div id="content" class="emoji">
+        <span>🥳</span>
+        <span>🎉</span>
+        <span>🎂</span>
+    </div>
+</div>
+
+<style>
+    /* general styling */
+    :root {
+        --smaller: 0.75;
+    }
+
+    * {
+        box-sizing: border-box;
+        margin: 0;
+        padding: 0;
+    }
+
+    html,
+    body {
+        height: 100%;
+        margin: 0;
+    }
+
+    body {
+        align-items: center;
+        background-color: #ffffff;
+        display: flex;
+        font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto,
+            Oxygen-Sans, Ubuntu, Cantarell, "Helvetica Neue", sans-serif;
+    }
+
+    .container {
+        color: #000000;
+        margin: 0 auto;
+        text-align: center;
+    }
+
+    h1 {
+        font-weight: normal;
+        letter-spacing: 0.125rem;
+        text-transform: uppercase;
+    }
+
+    li {
+        display: inline-block;
+        font-size: 1.5em;
+        list-style-type: none;
+        padding: 1em;
+        text-transform: uppercase;
+    }
+
+    li span {
+        display: block;
+        font-size: 4.5rem;
+    }
+
+    .emoji {
+        display: none;
+        padding: 1rem;
+    }
+
+    .emoji span {
+        font-size: 4rem;
+        padding: 0 0.5rem;
+    }
+
+    @media all and (max-width: 768px) {
+        h1 {
+            font-size: calc(1.5rem * var(--smaller));
+        }
+
+        li {
+            font-size: calc(1.125rem * var(--smaller));
+        }
+
+        li span {
+            font-size: calc(3.375rem * var(--smaller));
+        }
+    }
+</style>
