edf3c3cd44e78ea36a942e4e944d2e6358e8206c
Boof <97455552+hexahigh@users.noreply.github.com>
2023-09-14 08:52:25 +0000

    finished the confetti

--- src/lib/js/conf.js
+++ src/lib/js/conf.js
@@ -1,11 +1,97 @@
 export function fireAll() {
     fireMid()
+    fireLeft()
+    fireRight()
 }
 
 function fireMid() {
-    let count = 200,
+    let count = 100,
         defaults = {
             origin: { y: 0.7 },
+        };
+
+    function fire(particleRatio, opts) {
+        confetti(
+            Object.assign({}, defaults, opts, {
+                particleCount: Math.floor(count * particleRatio),
+            })
+        );
+    }
+
+    fire(0.25, {
+        spread: 26,
+        startVelocity: 55,
+    });
+
+    fire(0.2, {
+        spread: 60,
+    });
+
+    fire(0.35, {
+        spread: 100,
+        decay: 0.91,
+        scalar: 0.8,
+    });
+
+    fire(0.1, {
+        spread: 120,
+        startVelocity: 25,
+        decay: 0.92,
+        scalar: 1.2,
+    });
+
+    fire(0.1, {
+        spread: 120,
+        startVelocity: 45,
+    });
+};
+
+function fireLeft() {
+    let count = 100,
+        defaults = {
+            origin: { y: 0.7, x: 1 },
+        };
+
+    function fire(particleRatio, opts) {
+        confetti(
+            Object.assign({}, defaults, opts, {
+                particleCount: Math.floor(count * particleRatio),
+            })
+        );
+    }
+
+    fire(0.25, {
+        spread: 26,
+        startVelocity: 55,
+    });
+
+    fire(0.2, {
+        spread: 60,
+    });
+
+    fire(0.35, {
+        spread: 100,
+        decay: 0.91,
+        scalar: 0.8,
+    });
+
+    fire(0.1, {
+        spread: 120,
+        startVelocity: 25,
+        decay: 0.92,
+        scalar: 1.2,
+    });
+
+    fire(0.1, {
+        spread: 120,
+        startVelocity: 45,
+    });
+}
+
+function fireRight() {
+    let count = 100,
+        defaults = {
+            origin: { y: 0.7, x: 0 },
         };
 
     function fire(particleRatio, opts) {
