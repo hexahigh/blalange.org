3446f3ce014bb613f5dab220d6768b1a503aa821
hexahigh <97455552+hexahigh@users.noreply.github.com>
2023-09-16 17:28:44 +0200

    added images to the image page

--- src/routes/images/23.1/+page.svelte
+++ src/routes/images/23.1/+page.svelte
@@ -9,16 +9,19 @@
 <div class="main">
     <div class="row">
         <div class="column">
-
+            <img src="/img/231/2023-09-10_02.22.23.png">
+            <img src="/img/231/2023-09-11_18.41.28.png">
         </div>
         <div class="column">
-
+            <img src="/img/231/2023-09-13_17.22.52.png">
+            <img src="/img/231/2023-09-13_17.24.17.png">
         </div>
         <div class="column">
-
+            <img src="/img/231/2023-09-13_17.24.41.png">
+            <img src="/img/231/2023-09-16_17.23.42.png">
         </div>
         <div class="column">
-
+            <img src="/img/231/2023-09-16_17.23.53.png">
         </div>
     </div>
 </div>
