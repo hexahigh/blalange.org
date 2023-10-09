53609032930d7f5f4460ce2b0c9a88bb97b32700
hexahigh <97455552+hexahigh@users.noreply.github.com>
2023-10-09 15:16:24 +0200

    updated the image grid (i hate css)

--- src/routes/images/23.1/+page.svelte
+++ src/routes/images/23.1/+page.svelte
@@ -1,54 +1,78 @@
 <script>
-    import Nav from "../../Nav.svelte";
-    import ImagesNav from "$lib/components/images-nav.svelte";
+  import Nav from "../../Nav.svelte";
+  import ImagesNav from "$lib/components/images-nav.svelte";
 
-    import imagesData from "./images.json";
+  import imagesData from "./images.json";
 
-    let images = imagesData.slice(1); // Exclude the first object with hqUrl and lqUrl
+  let images = imagesData.slice(1); // Exclude the first object with hqUrl and lqUrl
 
-    function handleImageLoad(index) {
-        images[index].loaded = true;
-    }
+  function handleImageLoad(index) {
+    images[index].loaded = true;
+  }
+
+  // Distribute the images as evenly as possible into 4 columns
+  let columns = [[], [], [], []];
+  for (let i = 0; i < images.length; i++) {
+    columns[i % columns.length].push(images[i]);
+  }
 </script>
 
 <Nav />
 <ImagesNav />
 
 <div class="main">
-    <div class="image-container">
-        {#each images as image, index}
-            {#if image.loaded}
-                <img
-                    class="image-item"
-                    src={`${imagesData[0].hqUrl}${image.src}`}
-                    alt="High Resolution Image"
-                />
-            {:else}
-                <img
-                    class="image-item"
-                    src={`${imagesData[0].lqUrl}${image.src}`}
-                    alt="Low Resolution Image"
-                    on:load={() => handleImageLoad(index)}
-                />
-            {/if}
+  <div class="row">
+    {#each columns as column, columnIndex}
+      <div class="column">
+        {#each column as image, index}
+          <img
+            class="image-item"
+            src={`${imagesData[0].hqUrl}${image.src}`}
+            alt="High Resolution Image"
+          />
         {/each}
-    </div>
+      </div>
+    {/each}
+  </div>
 </div>
 
 <style>
-    .main {
-        background-color: #ffffff;
-    }
+  .main {
+    background-color: #ffffff;
+  }
 
-    .image-container {
-        display: flex;
-        flex-wrap: wrap;
-        justify-content: center;
-        gap: 10px;
-    }
+  .row {
+    display: flex;
+    flex-wrap: wrap;
+    padding: 0 4px;
+  }
 
-    .image-item {
-        flex: 1 0 200px;
-        max-width: 100%;
+  /* Create four equal columns that sits next to each other */
+  .column {
+    flex: 25%;
+    max-width: 25%;
+    padding: 0 4px;
+  }
+
+  .column img {
+    margin-top: 8px;
+    vertical-align: middle;
+    width: 100%;
+  }
+
+  /* Responsive layout - makes a two column-layout instead of four columns */
+  @media screen and (max-width: 800px) {
+    .column {
+      flex: 50%;
+      max-width: 50%;
     }
+  }
+
+  /* Responsive layout - makes the two columns stack on top of each other instead of next to each other */
+  @media screen and (max-width: 600px) {
+    .column {
+      flex: 100%;
+      max-width: 100%;
+    }
+  }
 </style>
