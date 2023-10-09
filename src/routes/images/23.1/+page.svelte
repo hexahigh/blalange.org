7eccf38d8b5895c405347d588d112efeff1ec538
Boof <97455552+hexahigh@users.noreply.github.com>
2023-10-09 10:28:57 +0000

    updated image page

--- src/routes/images/23.1/+page.svelte
+++ src/routes/images/23.1/+page.svelte
@@ -1,39 +1,34 @@
 <script>
     import Nav from "../../Nav.svelte";
     import ImagesNav from "$lib/components/images-nav.svelte";
+
+    import imagesData from "./images.json";
+
+    let images = imagesData.slice(1); // Exclude the first object with hqUrl and lqUrl
+
+    function handleImageLoad(index) {
+        images[index].loaded = true;
+    }
 </script>
 
 <Nav />
 <ImagesNav />
 
 <div class="main">
-    <div class="row">
-        <div class="column">
-            <img src="/img/image-archive/23.1/2023-09-10_02.22.23.png">
-            <img src="/img/image-archive/23.1/2023-09-11_18.41.28.png">
-            <img src="/img/image-archive/23.1/huge_2023-09-25_18.17.39.png">
-            <img src="/img/image-archive/23.1/huge_2023-09-25_18.18.53.png">
-            <img src="/img/image-archive/23.1/huge_2023-09-25_18.19.16.png">
-        </div>
-        <div class="column">
-            <img src="/img/image-archive/23.1/2023-09-13_17.22.52.png">
-            <img src="/img/image-archive/23.1/2023-09-13_17.24.17.png">
-            <img src="/img/image-archive/23.1/huge_2023-09-25_18.19.50.png">
-            <img src="/img/image-archive/23.1/huge_2023-09-25_18.20.14.png">
-        </div>
-        <div class="column">
-            <img src="/img/image-archive/23.1/2023-09-13_17.24.41.png">
-            <img src="/img/image-archive/23.1/2023-09-16_17.23.42.png">
-            <img src="/img/image-archive/23.1/huge_2023-09-25_18.20.30.png">
-            <img src="/img/image-archive/23.1/huge_2023-10-02_12.26.04.png">
-        </div>
-        <div class="column">
-            <img src="/img/image-archive/23.1/2023-09-16_17.23.53.png">
-            <img src="/img/image-archive/23.1/huge_2023-10-06_13.57.11.png">
-            <img src="/img/image-archive/23.1/huge_2023-10-06_13.57.29.png">
-            <img src="/img/image-archive/23.1/huge_2023-10-06_13.57.51.png">
-        </div>
-    </div>
+    {#each images as image, index}
+        {#if image.loaded}
+            <img
+                src={`${imagesData[0].hqUrl}${image.src}`}
+                alt="High Resolution Image"
+            />
+        {:else}
+            <img
+                src={`${imagesData[0].lqUrl}${image.src}`}
+                alt="Low Resolution Image"
+                on:load={() => handleImageLoad(index)}
+            />
+        {/if}
+    {/each}
 </div>
 
 <style>
@@ -41,35 +36,14 @@
         background-color: #ffffff;
     }
 
-    .row {
+    .main {
         display: flex;
-        flex-wrap: wrap;
-        padding: 0 4px;
     }
 
-    /* Create four equal columns that sits next to each other */
-    .column {
-        flex: 25%;
-        max-width: 25%;
-        padding: 0 4px;
-    }
-
-    .column img {
+    .main img {
         margin-top: 8px;
         vertical-align: middle;
-        width: 100%;
-    }
-
-    .column iframe {
-        margin-top: 8px;
-        vertical-align: middle;
-        width: 100%;
-    }
-
-    .column video {
-        margin-top: 8px;
-        vertical-align: middle;
-        width: 100%;
+        width: 25%;
     }
 
     /* Responsive layout - makes a two column-layout instead of four columns */
