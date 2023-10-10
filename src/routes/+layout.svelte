76482321e1916456cc6b0069bafee8f261255fc3
hexahigh <97455552+hexahigh@users.noreply.github.com>
2023-10-10 15:05:11 +0200

    so that is why it did not update

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,35 +2,8 @@
     import "../app.css";
     import "$lib/css/font.css";
     import "$lib/css/style2.css";
-    import { onMount } from "svelte";
-    import { pwaInfo } from "virtual:pwa-info";
 
-    onMount(async () => {
-        if (pwaInfo) {
-            const { registerSW } = await import("virtual:pwa-register");
-            registerSW({
-                immediate: true,
-                onRegistered(r) {
-                    // uncomment following code if you want check for updates
-                    // r && setInterval(() => {
-                    //    console.log('Checking for sw update')
-                    //    r.update()
-                    // }, 20000 /* 20s for testing purposes */)
-                    console.log(`SW Registered: ${r}`);
-                },
-                onRegisterError(error) {
-                    console.log("SW registration error", error);
-                },
-            });
-        }
-    });
-
-    $: webManifest = pwaInfo ? pwaInfo.webManifest.linkTag : "";
 </script>
-
-<svelte:head>
-    {@html webManifest}
-</svelte:head>
 
 <main>
     <slot />
