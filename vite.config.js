1f3dc607e4e3b6e0cc3760515188754da3712585
Boof <97455552+hexahigh@users.noreply.github.com>
2023-10-10 11:56:27 +0000

    added pwa icons

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,43 @@
 import { sveltekit } from '@sveltejs/kit/vite';
-import { SvelteKitPWA } from '@vite-pwa/sveltekit'
+import { SvelteKitPWA } from '@vite-pwa/sveltekit';
 
 /** @type {import('vite').UserConfig} */
 const config = {
   plugins: [
     sveltekit(),
-    SvelteKitPWA({/* pwa options */})
+    SvelteKitPWA({
+      manifest: {
+        name: 'Kukfest',
+        short_name: 'Kukfest',
+        start_url: '/',
+        icons: [
+          {
+            src: '/img/pwa-64x64.png',
+            sizes: '64x64',
+            type: 'image/png'
+          },
+          {
+            src: '/img/pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '/img/pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any'  
+          },
+          {
+            src: '/img/maskable-icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable'
+          }
+        ]
+      },
+      // Other PWA options...
+    }),
   ],
-}
+};
 
-export default config
+export default config;
