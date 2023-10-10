71cc6bcd14fa3ecc712658e2a9bc8a7d90ab68a5
hexahigh <97455552+hexahigh@users.noreply.github.com>
2023-10-10 15:14:56 +0200

    fixed vite-pwa overriding the manifest

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,38 @@
 <script>
-    import "../app.css";
-    import "$lib/css/font.css";
-    import "$lib/css/style2.css";
+  import "../app.css";
+  import "$lib/css/font.css";
+  import "$lib/css/style2.css";
+  import { onMount } from "svelte";
+  import { pwaInfo } from "virtual:pwa-info";
 
+  onMount(async () => {
+    if (pwaInfo) {
+      const { registerSW } = await import("virtual:pwa-register");
+      registerSW({
+        immediate: true,
+        onRegistered(r) {
+          // uncomment following code if you want check for updates
+          r &&
+            setInterval(() => {
+              console.log("Checking for sw update");
+              r.update();
+            }, 200000);
+          console.log(`SW Registered: ${r}`);
+        },
+        onRegisterError(error) {
+          console.log("SW registration error", error);
+        },
+      });
+    }
+  });
+
+  $: webManifest = pwaInfo ? pwaInfo.webManifest.linkTag : "";
 </script>
 
+<svelte:head>
+  {@html webManifest}
+</svelte:head>
+
 <main>
-    <slot />
+  <slot />
 </main>
--- vite.config.js
+++ vite.config.js
@@ -1,42 +1,14 @@
 import { sveltekit } from '@sveltejs/kit/vite';
-/*import { SvelteKitPWA } from '@vite-pwa/sveltekit';*/
+import { SvelteKitPWA } from '@vite-pwa/sveltekit';
 
 /** @type {import('vite').UserConfig} */
 const config = {
   plugins: [
     sveltekit(),
-    /*SvelteKitPWA({
-      manifest: {
-        name: 'Kukfest',
-        short_name: 'Kukfest',
-        start_url: '/',
-        icons: [
-          {
-            src: '/img/pwa-64x64.png',
-            sizes: '64x64',
-            type: 'image/png'
-          },
-          {
-            src: '/img/pwa-192x192.png',
-            sizes: '192x192',
-            type: 'image/png'
-          },
-          {
-            src: '/img/pwa-512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
-            purpose: 'any'  
-          },
-          {
-            src: '/img/maskable-icon-512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
-            purpose: 'maskable'
-          }
-        ]
-      },
+    SvelteKitPWA({
+      manifest: {}, // Disables automatic generation of manifest file
       // Other PWA options...
-    }),*/
+    }),
   ],
 };
 
