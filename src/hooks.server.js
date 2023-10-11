f5566c74a3355fa8c25fa90842e50c8354f25264
hexahigh <97455552+hexahigh@users.noreply.github.com>
2023-10-11 18:44:40 +0200

    does this work?

--- src/hooks.server.js
+++ src/hooks.server.js
@@ -1,13 +1,10 @@
-// src/hooks.js
+/** @type {import('@sveltejs/kit').Handle} */
 export async function handle({ event, resolve }) {
     // If the URL ends with 'sw.js', redirect to the actual service worker file
     if (event.url.pathname.endsWith('sw.js')) {
-      return {
-        status: 301,
-        headers: {
-          location: '/sw.js',
-        },
-      };
+        const response = await resolve(event);
+        response.headers.set('Location', '/sw.js');
+        response.status.set('301')
     }
   
     // Otherwise, continue with the normal request
