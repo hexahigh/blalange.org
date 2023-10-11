1083d2905e4b83cac930ceb66f859461ee77566d
Boof <97455552+hexahigh@users.noreply.github.com>
2023-10-11 09:48:57 +0000

    added external fonts to the cache

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,37 @@ const config = {
     SvelteKitPWA({
       strategies: 'generateSW',
       workbox: {
-        maximumFileSizeToCacheInBytes: 20000000
+        maximumFileSizeToCacheInBytes: 20000000,
+        runtimeCaching: [
+          {
+            urlPattern: /^https:\/\/fonts\.googleapis\.com\/.*/i,
+            handler: 'CacheFirst',
+            options: {
+              cacheName: 'google-fonts-cache',
+              expiration: {
+                maxEntries: 10,
+                maxAgeSeconds: 60 * 60 * 24 * 365 // <== 365 days
+              },
+              cacheableResponse: {
+                statuses: [0, 200]
+              }
+            }
+          },
+          {
+            urlPattern: /^https:\/\/fonts\.gstatic\.com\/.*/i,
+            handler: 'CacheFirst',
+            options: {
+              cacheName: 'gstatic-fonts-cache',
+              expiration: {
+                maxEntries: 10,
+                maxAgeSeconds: 60 * 60 * 24 * 365 // <== 365 days
+              },
+              cacheableResponse: {
+                statuses: [0, 200]
+              },
+            }
+          }
+        ]
       }
       // Other PWA options...
     }),
