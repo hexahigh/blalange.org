19d916f757ff26359635507389f96734bbd3d142
hexahigh <97455552+hexahigh@users.noreply.github.com>
2023-10-16 16:00:03 +0200

    blocked

--- src/hooks.server.js
+++ src/hooks.server.js
@@ -1,38 +1,13 @@
 /** @type {import('@sveltejs/kit').Handle} */
 export async function handle({ event, resolve }) {
-  // If the URL has a url settings thing then archive the page
-  if (event.url.searchParams.has("archive")) {
-    try {
-      let inputUrl = event.url;
+  let ip = event.request.headers.get("CF-Connecting-IP");
+  let blocked = ["84.215.200.49"];
 
-      if (event.url.searchParams.has("ia")) {
-      // Archive to the Wayback Machine
-        archiveIA(inputUrl)
-    }
-
-    if (event.url.searchParams.has("today")) {
-      // Archive to archive.today
-      archiveToday(inputUrl)
-    } else {
-
-    }
-  } catch (error) {
-    return new Response(error);
+  if (blocked.includes(ip)) {
+    return new Response("Haha get blocked");
   }
-}
-  // Otherwise, continue with the normal request
+
   const response = await resolve(event);
+
   return response;
-}
-
-
-async function archiveToday(url) {
-    await fetch(`https://archive.ph/submit/?url=${url}`);
-}
-
-async function archiveIA(url) {
-    await fetch(`https://web.archive.org/save/${url}`, {
-        body: `url=${encodeURIComponent(url)}&capture_all=on`,
-        method: "POST",  
-      });
 }
