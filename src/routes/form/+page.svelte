74e1bc45c7177479f03599991b842f8d558f67b1
Boof <97455552+hexahigh@users.noreply.github.com>
2023-10-25 12:13:55 +0000

    updated form

--- src/routes/form/+page.svelte
+++ src/routes/form/+page.svelte
@@ -1,46 +1,98 @@
 <script>
-  import {
-    Label,
-    Input,
-    InputAddon,
-    ButtonGroup,
-    Radio,
-  } from "flowbite-svelte";
+  import autoAnimate from '@formkit/auto-animate';
   import {
     EnvelopeSolid,
     UserCircleSolid,
     QuestionCircleSolid,
   } from "flowbite-svelte-icons";
-  let inline1 = 'second';
+
+  let email = '';
+  let name = '';
+  let yesOrNo = '';
+  let secondYesOrNo = '';
+  let additionalInput1 = '';
+  let additionalInput2 = '';
+  let image = null;
+  
+  function handleSubmit() {
+    // Handle form submission logic here
+  }
+  
+  function clearImage() {
+    image = null;
+  }
 </script>
 
-<form class="ml-12">
-  <div class="mb-6">
-    <Label for="input-group-1" class="block mb-2">Your Email</Label>
-    <ButtonGroup class="w-full inline-flex rounded-lg shadow-sm">
-      <InputAddon class="text-sm px-3 border-gray-300 dark:border-gray-600 text-gray-500 bg-gray-200 dark:bg-gray-600 dark:text-gray-400 dark:border-r-gray-700 dark:last:border-r-gray-600 inline-flex items-center border-t border-b first:border-l border-r first:rounded-l-lg last:rounded-r-lg">
-        <EnvelopeSolid class="w-4 h-4 text-gray-500 dark:text-gray-400" />
-      </InputAddon>
-      <Input id="email" type="email" placeholder="john@example.com" class="block w-full disabled:cursor-not-allowed disabled:opacity-50 p-2.5 focus:border-primary-500 focus:ring-primary-500 dark:focus:border-primary-500 dark:focus:ring-primary-500 bg-gray-50 text-gray-900 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 border-gray-300 dark:border-gray-600 text-sm first:rounded-l-lg last:rounded-r-lg border-l-0 first:border-l last:border-r" />
-    </ButtonGroup>
-  </div>
-  <div class="mb-6">
-    <Label for="name" class="block mb-2">Name</Label>
-    <ButtonGroup class="w-full inline-flex">
-      <InputAddon>
-        <UserCircleSolid class="w-4 h-4 text-gray-500 dark:text-gray-400" />
-      </InputAddon>
-      <Input id="name" placeholder="John Doe" required />
-    </ButtonGroup>
-  </div>
-  <div class="mb-6">
-  <Label class="block mb-2"></Label>
-    <div class="flex gap-3">
-      <InputAddon>
-        <QuestionCircleSolid class="w-4 h-4 text-gray-500 dark:text-gray-400" />
-      </InputAddon>
-      <Radio group={inline1} name={inline1} value="yes" class="mr-2" color="orange">Yes</Radio>
-      <Radio group={inline1} name={inline1} value="no" class="mr-2" color="orange">No</Radio>
+<form class="flex flex-col m-8" on:submit|preventDefault={handleSubmit} use:autoAnimate>
+  <label class="flex flex-col mb-4">
+    <span class="mb-2">Email</span>
+    <div class="flex items-center">
+      <EnvelopeSolid class="h-5 w-5 mr-2 text-orange-500"/>
+      <input bind:value={email} class="border-2 border-orange-500 p-2 w-full rounded-md" type="email" placeholder="john@example.com" required>
+    </div>
+  </label>
+  
+  <label class="flex flex-col mb-4">
+    <span class="mb-2">Name</span>
+    <div class="flex items-center">
+      <UserCircleSolid class="h-5 w-5 mr-2 text-orange-500" />
+      <input bind:value={name} class="border-2 border-orange-500 p-2 w-full rounded-md" type="text" placeholder="John Doe" required>
+    </div>
+  </label>
+  
+  <div class="mb-4">
+    <span class="mb-2">Yes or No?</span>
+    <div class="flex items-center">
+      <label class="flex items-center mr-4">
+        <QuestionCircleSolid class="h-5 w-5 mr-2 text-orange-500"/>
+        <input bind:group={yesOrNo} class="mr-2" type="radio" value="yes">
+        <span>Yes</span>
+      </label>
+      <label class="flex items-center">
+        <input bind:group={yesOrNo} class="mr-2" type="radio" value="no">
+        <span>No</span>
+      </label>
     </div>
   </div>
+
+  <div class="mb-4">
+    <span class="mb-2">Second Yes or No?</span>
+    <div class="flex items-center">
+      <label class="flex items-center mr-4">
+        <QuestionCircleSolid class="h-5 w-5 mr-2 text-orange-500"/>
+        <input bind:group={secondYesOrNo} class="mr-2" type="radio" value="yes">
+        <span>Yes</span>
+      </label>
+      <label class="flex items-center">
+        <input bind:group={secondYesOrNo} class="mr-2" type="radio" value="no">
+        <span>No</span>
+      </label>
+    </div>
+  </div>
+
+  {#if secondYesOrNo === 'yes'}
+    <label class="flex flex-col mb-4">
+      <span class="mb-2">Additional Input 1</span>
+      <input bind:value={additionalInput1} class="border-2 border-orange-500 p-2 w-full rounded-md" type="text" placeholder="Additional Input 1">
+    </label>
+
+    <label class="flex flex-col mb-4">
+      <span class="mb-2">Additional Input 2</span>
+      <input bind:value={additionalInput2} class="border-2 border-orange-500 p-2 w-full rounded-md" type="text" placeholder="Additional Input 2">
+    </label>
+
+    <label class="flex flex-col mb-4">
+      <span class="mb-2">Image</span>
+      {#if image}
+        <div class="flex items-center">
+          <img src={URL.createObjectURL(image)} alt="Uploaded Image" class="h-20 w-20 mr-2 rounded-md">
+          <button class="bg-red-500 text-white p-2 rounded-md" on:click={clearImage}>Clear Image</button>
+        </div>
+      {:else}
+        <input type="file" accept="image/*" on:change={(e) => { image = e.target.files[0]; }} class="border-2 border-orange-500 p-2 w-full rounded-md">
+      {/if}
+    </label>
+  {/if}
+  <button class="bg-orange-500 text-white p-2 mt-4 rounded-md" type="submit">Submit</button>
 </form>
+
