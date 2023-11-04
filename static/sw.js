b0abaf83e4434063b64606c15a96e0293a1272dc
hexahigh <97455552+hexahigh@users.noreply.github.com>
2023-11-04 14:29:24 +0100

    update

--- static/sw.js
+++ static/sw.js
@@ -2,6 +2,7 @@ import { skipWaiting, clientsClaim } from 'workbox-core';
 import { precacheAndRoute, createHandlerBoundToURL } from 'workbox-precaching';
 import { registerRoute } from 'workbox-routing';
 import { NetworkFirst } from 'workbox-strategies';
+import { CacheableResponsePlugin } from 'workbox-cacheable-response';
 
 skipWaiting();
 clientsClaim();
