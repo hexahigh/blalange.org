177d24995ebd686732439e8b07578c072032b45f
Boofdev <hexahigh0@gmail.com>
2023-12-14 11:54:58 +0100

    updated form viewer

--- src/routes/form/view/+page.svelte
+++ src/routes/form/view/+page.svelte
@@ -1,22 +1,34 @@
 <script>
-    import { onMount } from 'svelte';
-    import PocketBase from 'pocketbase';
-  
-    let data = [];
-  
-    onMount(async () => {
-      const pb = new PocketBase("https://db.080609.xyz");
-      data = await pb.collection("form").get();
+  import { onMount } from "svelte";
+  import PocketBase from "pocketbase";
+
+  let data = [];
+
+  onMount(async () => {
+    const pb = new PocketBase("https://db.080609.xyz");
+    data = await pb.collection("form").getFullList(200 /* batch size */, {
+      sort: "-created",
     });
-  </script>
-  
-  {#each data as item (item.id)}
-    <div>
-      <p>{item.name}</p>
-      <p>{item.email}</p>
-      <p>{item.extra}</p>
-      <p>{item.age}</p>
-      <p>{item.filmed}</p>
-      <p>{item.MW}</p>
-    </div>
-  {/each}
+  });
+</script>
+
+<style>
+  .data-item {
+    border: 1px solid #ddd;
+    padding: 10px;
+    margin-bottom: 10px;
+    border-radius: 5px;
+  }
+</style>
+
+{#each data as item (item.id)}
+  <div class="data-item">
+    <p><strong>Name:</strong> {item.name}</p>
+    <p><strong>Email:</strong> {item.email}</p>
+    <p><strong>Extra:</strong> {item.extra}</p>
+    <p><strong>Age:</strong> {item.age}</p>
+    <p><strong>LatLon:</strong> {item.latlon}</p>
+    <p><strong>Filmed:</strong> {item.filmed}</p>
+    <p><strong>MW:</strong> {item.MW}</p>
+  </div>
+{/each}
