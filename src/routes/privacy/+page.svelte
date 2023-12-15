c56c8b422094ec966336c489c76767248a104a96
Boofdev <admin@boofdev.eu>
2023-12-15 20:30:48 +0100

    updated privacy policy

--- src/routes/privacy/+page.svelte
+++ src/routes/privacy/+page.svelte
@@ -1,9 +1,9 @@
 <div class="text-center">
-  <h1><s>Privacy</s> Policy</h1>
+  <h1 class="text-3xl"><s>Privacy</s> Policy</h1>
 
-  <p>At Kukfest, we (dont) respect your privacy and are committed to protecting it. This Privacy Policy explains what information we collect, how we use it, and your rights in relation to it.</p>
+  <p>At Kukfest, we (dont) respect your privacy and are (not) committed to protecting it. This Privacy Policy explains what information we collect, how we use it, and your rights in relation to it.</p>
   
-  <h2>What Information We Collect</h2>
+  <h2 class="text-2xl">What Information We Collect</h2>
   
   <p>We collect the following information when you use our services:</p>
   
@@ -20,15 +20,15 @@
     <li>Naked photos (only if you are a woman and over 18)</li>
   </ul>
   
-  <h2>How We Use Your Information</h2>
+  <h2 class="text-2xl">How We Use Your Information</h2>
   
   <p>We use your information to provide and improve our services. We do not share your personal information with third parties without your consent, except to comply with laws or to protect our rights. (Or if we feel like doing a litte doxing)</p>
   
-  <h2>Your Rights</h2>
+  <h2 class="text-2xl">Your Rights</h2>
   
   <p>You have the right to access, update, or delete your personal information at any time. If you have any questions about this Privacy Policy, please contact us.</p>
   
-  <h2>Changes to This Privacy Policy</h2>
+  <h2 class="text-2xl">Changes to This Privacy Policy</h2>
   
   <p>We may update our Privacy Policy from time to time. We will notify you of any changes by posting the new Privacy Policy on this page.</p>
   
