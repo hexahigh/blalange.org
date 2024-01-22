5dce114532a5204d55fdde0bb2487ea9f8b53258
Boof <97455552+hexahigh@users.noreply.github.com>
2024-01-22 23:06:21 +0100

    Create +page.svelte

---
+++ src/routes/webring/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+ import sites from './data.json';
+</script>
+
+<div class="grid grid-cols-3 gap-4">
+ {#each sites as site (site.id)}
+    <a href={site.url}>
+      <img src={site.imageUrl} alt={site.name} class="object-cover w-full h-auto" />
+    </a>
+ {/each}
+</div>
