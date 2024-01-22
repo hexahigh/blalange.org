46716393d28461cb910ee93c4ceb2e13ad5b96cb
Boofdev <hexahigh0@gmail.com>
2024-01-22 10:06:31 +0100

    add people component

---
+++ src/lib/components/people.svelte
@@ -0,0 +1,9 @@
+<script>
+    import h from '$lib/img/people/herremann.jpg'
+</script>
+
+<div class="flex flex-col items-center">
+    <img src={h} alt="alt" class="w-full h-auto rounded-lg shadow-md mb-4"/>
+    <h2 class="text-xl font-bold mb-2">Lead designer</h2>
+    <p class="text-sm text-gray-600">bio</p>
+</div>
