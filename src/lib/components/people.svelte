6814de43a3bfd4acadea50b30fa2f31a060c81fc
Boofdev <hexahigh0@gmail.com>
2024-01-22 11:47:06 +0100

    update css

--- src/lib/components/people.svelte
+++ src/lib/components/people.svelte
@@ -8,12 +8,12 @@
         <img src={h} alt="Hermann Roos" class="w-full h-auto rounded-lg shadow-md mb-4"/>
         <h1 class="text-2xl font-bold mb-2">Hermann Roos</h1>
         <h2 class="text-xl font-bold mb-2">Øverste visjoner offiser</h2>
-        <p class="text-sm text-gray-600">bio</p>
+        <p class="text-sm">bio</p>
     </div>
     <div class="flex flex-col items-center w-full md:w-1/2 lg:w-1/3 xl:w-1/4 p-4">
         <img src={u} alt="Simon Bråten" class="w-full h-auto rounded-lg shadow-md mb-4"/>
         <h1 class="text-2xl font-bold mb-2">Simon Bråten</h1>
         <h2 class="text-xl font-bold mb-2">Hoved utvikler</h2>
-        <p class="text-sm text-gray-600">bio</p>
+        <p class="text-sm">bio</p>
     </div>
 </div>
