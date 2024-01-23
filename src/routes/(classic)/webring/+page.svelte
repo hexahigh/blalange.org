2151bf2172ea52109f0392c2a9349c327deeceb9
Boofdev <hexahigh0@gmail.com>
2024-01-23 08:58:01 +0100

    update webring

---
+++ src/routes/(classic)/webring/+page.svelte
@@ -0,0 +1,15 @@
+<script>
+  import sites from "./data.json";
+</script>
+
+<div class="grid grid-cols-3 gap-4 bg-black">
+  {#each sites as site (site.id)}
+    <a href={site.url}>
+      <img
+        src={site.imageUrl}
+        alt={site.name}
+        class="object-cover w-full h-auto"
+      />
+    </a>
+  {/each}
+</div>
