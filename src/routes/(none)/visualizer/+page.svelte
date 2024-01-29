2b4c891e65cc00e793f03131559c45821a037f52
Boofdev <hexahigh0@gmail.com>
2024-01-29 10:00:49 +0100

    add visualizer

---
+++ src/routes/(none)/visualizer/+page.svelte
@@ -0,0 +1,38 @@
+<script>
+    import AudioMotionAnalyzer from "audiomotion-analyzer";
+    import { onMount } from "svelte";
+   
+    let audioData;
+   
+    onMount(async () => {
+       const audioMotion = new AudioMotionAnalyzer(
+         document.getElementById("container"),
+         {
+           source: document.getElementById("audio"),
+           radial: true,
+           showFPS: true,
+           spinSpeed: 2,
+           channelLayout: "dual-horizontal"
+         }
+       );
+    });
+   
+    function handleFileChange(event) {
+       const file = event.target.files[0];
+       const reader = new FileReader();
+   
+       reader.onload = function(e) {
+         const audioElement = document.getElementById('audio');
+         audioElement.src = e.target.result;
+         audioElement.play();
+       };
+   
+       reader.readAsDataURL(file);
+    }
+   </script>
+   
+   <div class="w-full h-full">
+       <input id="audioInput" type="file" on:change="{handleFileChange}" />
+       <audio id="audio" controls></audio>
+       <div id="container" class="h-screen"></div>
+   </div>
