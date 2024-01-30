c46b4a2f3b359a56c93bcc509103fb268f1a8ac0
Boofdev <hexahigh0@gmail.com>
2024-01-30 13:55:39 +0100

    update articles

--- src/routes/articles/+page.svelte
+++ src/routes/articles/+page.svelte
@@ -2,12 +2,23 @@
   import Nav from "../Nav.svelte";
   import { Card, Button } from "flowbite-svelte";
   import cicignonPreview from "$lib/img/article-previews/cicignon.jpg";
+  import herremann from "$lib/img/people/herremann.jpg"
 </script>
 
 <!--<nav />-->
-<div class="w-full mx-auto bg-gradient-to-r bg-white dark:bg-gray-900 p-6 flex justify-center flex-wrap gap-2">
-  <div class="space-y-4">
-<Card img={cicignonPreview} href="/" size="md">
+<div class="w-full mx-auto bg-gradient-to-r bg-white dark:bg-gray-900 p-6 grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-2">
+  <Card img={cicignonPreview} href="/" size="md">
+     <h5
+       class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"
+     >
+       Noteworthy technology acquisitions 2021
+     </h5>
+     <p class="mb-3 font-normal text-gray-700 dark:text-gray-400 leading-tight">
+       Here are the biggest enterprise technology acquisitions of 2021 so far, in
+       reverse chronological order.
+     </p>
+  </Card>
+  <Card img={herremann} href="/" size="md" class="w-[200px] h-[200px]">
     <h5
       class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"
     >
@@ -17,6 +28,6 @@
       Here are the biggest enterprise technology acquisitions of 2021 so far, in
       reverse chronological order.
     </p>
-  </Card>
-  </div>
-</div>
+ </Card>
+  <!-- Add more Card components here -->
+ </div>
