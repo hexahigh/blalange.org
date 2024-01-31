6de14b8c1a4493c3127a315ba40cfa4f13b774ed
Boofdev <hexahigh0@gmail.com>
2024-01-31 10:33:08 +0100

    update articles

--- src/routes/articles/+page.svelte
+++ src/routes/articles/+page.svelte
@@ -2,23 +2,23 @@
   import Nav from "../Nav.svelte";
   import { Card, Button } from "flowbite-svelte";
   import cicignonPreview from "$lib/img/article-previews/cicignon.jpg";
-  import herremann from "$lib/img/people/herremann.jpg"
+  import herremann from "$lib/img/people/herremann.jpg";
+  import ArticleCard from "$lib/components/articleCard.svelte"
 </script>
 
 <nav />
-<div class="w-full mx-auto bg-gradient-to-r bg-white dark:bg-gray-900 p-6 grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-2">
-  <a href="#" class="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
-    <img class="object-cover w-full rounded-t-lg h-96 md:w-48 md:rounded-none md:rounded-s-lg" src={cicignonPreview} alt="">
-    <div class="flex flex-col justify-between p-4 leading-normal">
-        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
-        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
-    </div>
-</a>
-  <a href="#" class="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
-    <img class="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-s-lg" src={herremann} alt="">
-    <div class="flex flex-col justify-between p-4 leading-normal">
-        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Ballemann sine baller falt av</h5>
-        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Lorem ipsum oppi rumpa mi</p>
-    </div>
-</a>
- </div>
+<div
+  class="w-full mx-auto bg-gradient-to-r bg-white dark:bg-gray-900 p-6 grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-2"
+>
+<ArticleCard image={cicignonPreview} title="lorem ipsum" description="lorem ipsum" link="https://example.com"/>
+<ArticleCard image={cicignonPreview} title="lorem ipsum" description="lorem ipsum" link="https://example.com"/>
+<ArticleCard image={cicignonPreview} title="lorem ipsum" description="lorem ipsum" link="https://example.com"/>
+<ArticleCard image={cicignonPreview} title="lorem ipsum" description="lorem ipsum" link="https://example.com"/>
+<ArticleCard image={cicignonPreview} title="lorem ipsum" description="lorem ipsum" link="https://example.com"/>
+<ArticleCard image={cicignonPreview} title="lorem ipsum" description="lorem ipsum" link="https://example.com"/>
+<ArticleCard image={cicignonPreview} title="lorem ipsum" description="lorem ipsum" link="https://example.com"/>
+<ArticleCard image={cicignonPreview} title="lorem ipsum" description="lorem ipsum" link="https://example.com"/>
+<ArticleCard image={cicignonPreview} title="lorem ipsum" description="lorem ipsum" link="https://example.com"/>
+<ArticleCard image={cicignonPreview} title="lorem ipsum" description="lorem ipsum" link="https://example.com"/>
+
+</div>
