50db2195acf1e009cce595171694456f9946be8a
Boofdev <hexahigh0@gmail.com>
2024-01-31 15:36:26 +0100

    udpate

--- src/routes/articles/+page.svelte
+++ src/routes/articles/+page.svelte
@@ -8,7 +8,7 @@
 
 <nav />
 <div
-  class="w-full mx-auto bg-gradient-to-r bg-white dark:bg-gray-900 p-6 grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-2"
+  class="w-full mx-auto bg-gradient-to-r bg-white dark:bg-gray-900 p-6 grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-2"
 >
   <ArticleCard
     image={cicignonPreview}
