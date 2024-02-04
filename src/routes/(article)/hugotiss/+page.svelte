eb0172cabf869930335baa772a95a5fbffd5d539
Boofdev <hexahigh0@gmail.com>
2024-02-04 16:34:52 +0100

    ballemann prokrastiner

--- src/routes/(article)/hugotiss/+page.svelte
+++ src/routes/(article)/hugotiss/+page.svelte
@@ -3,7 +3,7 @@
   import cicignonPreview from "$lib/img/article-previews/cicignon.jpg";
   import calendar from "$lib/svg/calendar.svg";
   import person from "$lib/svg/person.svg";
-  import hugo from "$lib/img/people/hugo.jpg"
+  import hugo from "$lib/img/people/hugo.jpg";
 </script>
 
 <MetaTags
@@ -49,28 +49,31 @@
         sex i gutte garderoben på Cicignon skole. Dette var visst ikke første
         gangen de hadde gjort det her som Hugo senere forklarte i et intervju.
       </p>
-      <figure class="max-w-screen-md mx-auto text-center">
-        <svg class="w-8 h-8 text-gray-400 dark:text-gray-600 mb-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 18 14">
-          <path d="M6 0H2a2 2 0 0 0-2 2v4a2 2 0 0 0 2 2h4v1a3 3 0 0 1-3 3H2a1 1 0 0 0 0 2h1a5.006 5.006 0 0 0 5-5V2a2 2 0 0 0-2-2Zm10 0h-4a2 2 0 0 0-2 2v4a2 2 0 0 0 2 2h4v1a3 3 0 0 1-3 3h-1a1 1 0 0 0 0 2h1a5.006 5.006 0 0 0 5-5V2a2 2 0 0 0-2-2Z"/>
-      </svg>
-        <div>
-          <p class="text-2xl italic font-medium text-gray-900 dark:text-white">
-            Jeg kom
-          </p>
-        </div>
-        <figcaption
-          class="flex items-center justify-center mt-6 space-x-3 rtl:space-x-reverse"
+      <div
+        class="p-4 my-4 border-s-4 border-gray-300 bg-gray-50 dark:border-gray-500 dark:bg-gray-800"
+      >
+        <svg
+          class="w-8 h-8 text-gray-400 dark:text-gray-600 mb-4"
+          aria-hidden="true"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="currentColor"
+          viewBox="0 0 18 14"
         >
-          <img
-            class="w-6 h-6 rounded-full"
-            src={hugo}
-            alt="HUGOOO"
+          <path
+            d="M6 0H2a2 2 0 0 0-2 2v4a2 2 0 0 0 2 2h4v1a3 3 0 0 1-3 3H2a1 1 0 0 0 0 2h1a5.006 5.006 0 0 0 5-5V2a2 2 0 0 0-2-2Zm10 0h-4a2 2 0 0 0-2 2v4a2 2 0 0 0 2 2h4v1a3 3 0 0 1-3 3h-1a1 1 0 0 0 0 2h1a5.006 5.006 0 0 0 5-5V2a2 2 0 0 0-2-2Z"
           />
-            <cite class="pe-3 font-medium text-gray-900 dark:text-white"
-              >Hugo Holmquist</cite
-            >
-        </figcaption>
-      </figure>
+        </svg>
+        <p
+          class="text-xl italic font-medium leading-relaxed text-gray-900 dark:text-white"
+        >
+          "Jeg kom"
+        </p>
+        <p
+          class="text-l italic font-medium leading-relaxed text-gray-900 dark:text-white"
+        >
+          -Hugo Holmquist
+        </p>
+      </div>
     </article>
   </div>
 </div>
