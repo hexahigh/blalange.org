484332ba1a7d96a744a2b7b35d4b57101fff9f5d
Boofdev <hexahigh0@gmail.com>
2024-03-16 15:38:53 +0100

    cat error

---
+++ src/routes/api/error/[slug]/+page.server.js
@@ -0,0 +1,11 @@
+/** @type {import('./$types').RequestHandler} */
+
+import { error } from '@sveltejs/kit';
+
+export async function load({ request, params }) {
+
+    
+
+    throw error(params.slug, 'Error test');
+
+}
