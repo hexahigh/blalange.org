313d86b3a2d1589b063aab93e0eae8025330e35c
Boofdev <hexahigh0@gmail.com>
2024-03-16 16:32:41 +0100

    nvm

--- src/routes/api/error/[slug]/+page.server.js
+++ src/routes/api/error/[slug]/+page.server.js
@@ -4,8 +4,5 @@ import { error } from '@sveltejs/kit';
 
 export async function load({ request, params }) {
 
-    
-    return new Redirect(params.slug, 'Error test');
-
     throw error(params.slug, 'Error test');
 }
