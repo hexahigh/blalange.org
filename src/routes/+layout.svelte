748e712a3b9dc6b795c04c08d55c0295106b7bac
Boofdev <hexahigh0@gmail.com>
2024-03-16 13:30:30 +0100

    update service worker

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -9,10 +9,38 @@
   import "@fontsource/roboto-condensed";
   import '@fontsource/material-icons';
 
+  import { pwaInfo } from 'virtual:pwa-info'; 
+
+  onMount(async () => {
+    if (pwaInfo) {
+      const { registerSW } = await import('virtual:pwa-register')
+      registerSW({
+        immediate: true,
+        onRegistered(r) {
+          // uncomment following code if you want check for updates
+          // r && setInterval(() => {
+          //    console.log('Checking for sw update')
+          //    r.update()
+          // }, 20000 /* 20s for testing purposes */)
+          console.log(`SW Registered: ${r}`)
+        },
+        onRegisterError(error) {
+          console.log('SW registration error', error)
+        }
+      })
+    }
+  })
+
+  $: webManifestLink = pwaInfo ? pwaInfo.webManifest.linkTag : '' 
+
   onMount(() => {
     startAnalyticsMonitoring();
   });
 </script>
+
+<svelte:head> 
+ 	{@html webManifestLink} 
+</svelte:head>
 
 <main>
   <slot />
--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,22 @@
 import { sveltekit } from '@sveltejs/kit/vite';
+import { SvelteKitPWA } from '@vite-pwa/sveltekit'
 
 /** @type {import('vite').UserConfig} */
 const config = {
   plugins: [
     sveltekit(),
+    SvelteKitPWA({
+      strategies: 'injectManifest',
+      srcDir: 'src',
+      filename: 'sw.js',
+      /* other pwa options */  
+    })
   ],
+  define: {
+    'process.env.NODE_ENV': process.env.NODE_ENV === 'production' 
+      ? '"production"'
+      : '"development"'
+  }
 };
 
 export default config;
