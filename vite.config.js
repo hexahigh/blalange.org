2ab35059baa203a512497c163a1c796dbeaed17d
Boofdev <hexahigh0@gmail.com>
2024-03-16 13:44:28 +0100

    add manifest in vite file

--- vite.config.js
+++ vite.config.js
@@ -6,11 +6,52 @@ const config = {
   plugins: [
     sveltekit(),
     SvelteKitPWA({
-      strategies: 'generateSW',
+      strategies: 'injectManifest',
       srcDir: 'src',
       filename: 'sw.js',
       manifestFilename: 'manifest.json',
-      /* other pwa options */  
+      manifest: {
+          "name": "Blålange festivalen",
+          "short_name": "Blålange",
+          "start_url": "/",
+          "display": "standalone",
+          "background_color": "#ffffff",
+          "lang": "nb",
+          "scope": "./",
+          "icons": [
+            {
+              "src": "/img/pwa-64x64.png",
+              "sizes": "64x64",
+              "type": "image/png"
+            },
+            {
+              "src": "/img/pwa-192x192.png",
+              "sizes": "192x192",
+              "type": "image/png"
+            },
+            {
+              "src": "/img/pwa-512x512.png",
+              "sizes": "512x512",
+              "type": "image/png",
+              "purpose": "any"
+            },
+            {
+              "src": "/img/maskable-icon-512x512.png",
+              "sizes": "512x512",
+              "type": "image/png",
+              "purpose": "maskable"
+            }
+          ],
+          "theme_color": "#000000",
+          "id": "kuk-2023-9921832113",
+          "description": "Blålange Festivalen er en norsk festival som skjer et par ganger i áret.",
+          "dir": "auto",
+          "categories": [
+            "news",
+            "social",
+            "events"
+          ]
+      }
     })
   ],
   define: {
