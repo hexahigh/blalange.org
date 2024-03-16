d1145273fc4146b9d19d5ecf02cc3d3bc9effc03
Boofdev <hexahigh0@gmail.com>
2024-03-16 16:31:28 +0100

    better

--- src/routes/api/error/[slug]/+page.server.js
+++ src/routes/api/error/[slug]/+page.server.js
@@ -5,7 +5,7 @@ import { error } from '@sveltejs/kit';
 export async function load({ request, params }) {
 
     
+    return new Redirect(params.slug, 'Error test');
 
     throw error(params.slug, 'Error test');
-
 }
