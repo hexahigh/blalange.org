62c0a0b75519f594eb6127395bf4f48f37543e17
Boofdev <hexahigh0@gmail.com>
2024-03-17 16:24:31 +0100

    add commit hash to version num

--- src/lib/components/footer.svelte
+++ src/lib/components/footer.svelte
@@ -1,5 +1,6 @@
 <script>
     import config from "$lib/json/config.json";
+    let commitHash = __COMMIT_HASH__;
 </script>
 
 <footer class="w-full text-center border-t border-grey p-4 pin-b bg-gray-200">
@@ -7,6 +8,6 @@
         <a href="https://github.com/hexahigh/kukfest" class="hover:underline">Kildekode</a>
         <a href="mailto:webmaster@blalange.org" class="hover:underline ml-4">Epost</a>
     </div>
-    <p>Versjon {config.version.id} - {config.version.name}</p>
+    <p>Versjon {config.version.id}.{commitHash} - {config.version.name}</p>
     <p>{new Date().getFullYear()} © Blålange Festivalen. All rights reserved.</p>
 </footer>
