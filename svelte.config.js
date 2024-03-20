ed98c2a066b39e2293bbc111ce7b5c0ec9fa181c
Boofdev <hexahigh0@gmail.com>
2024-03-20 18:16:32 +0100

    Revert "md"
    This reverts commit b7390177ae844dd10fbe808901891bdb86a0da90.

--- svelte.config.js
+++ svelte.config.js
@@ -1,16 +1,8 @@
 import adapter from "@sveltejs/adapter-auto";
 import { vitePreprocess } from "@sveltejs/kit/vite";
-import { mdsvex } from 'mdsvex'
-
-/** @type {import('mdsvex').MdsvexOptions} */
-const mdsvexOptions = {
-	extensions: ['.md'],
-}
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  extensions: ['.svelte', '.md'],
-  preprocess: [vitePreprocess(), mdsvex(mdsvexOptions)],
   kit: {
     // adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
     // If your environment is not supported or you settled on a specific environment, switch out the adapter.
@@ -23,6 +15,7 @@ const config = {
       register: false,
     },
   },
+  preprocess: vitePreprocess(),
 };
 
 export default config;
