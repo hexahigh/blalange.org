a0929c17b2e8b9e83a0f20d257cba0691ee27eae
Boofdev <hexahigh0@gmail.com>
2024-03-21 19:11:43 +0100

    bump version

--- src/lib/components/footer.svelte
+++ src/lib/components/footer.svelte
@@ -1,6 +1,13 @@
 <script>
     import config from "$lib/json/config.json";
     let commitHash = __COMMIT_HASH__;
+
+    function getLatestVersion() {
+        const latestVersion = config.versions[config.versions.length - 1];
+        return `${latestVersion.id}.${latestVersion.name}`;
+    }
+
+    let latestVersion = getLatestVersion();
 </script>
 
 <footer class="w-full text-center border-t border-grey p-4 pin-b bg-gray-200">
@@ -8,6 +15,6 @@
         <a href="https://github.com/hexahigh/kukfest" class="hover:underline">Kildekode</a>
         <a href="mailto:webmaster@blalange.org" class="hover:underline ml-4">Epost</a>
     </div>
-    <p>Versjon {config.version.id}.{commitHash} - {config.version.name}</p>
+    <p>Versjon {latestVersion} - {commitHash}</p>
     <p>{new Date().getFullYear()} © Blålange Festivalen. All rights reserved.</p>
 </footer>
