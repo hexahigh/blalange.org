c80c12a3ba3bbde666ebdd3b71c089959945d896
Boofdev <hexahigh0@gmail.com>
2024-03-22 10:08:28 +0100

    catppuccin

--- src/lib/components/nav2.svelte
+++ src/lib/components/nav2.svelte
@@ -81,10 +81,10 @@
    
    <style lang="postcss">
     .current-page {
-       @apply block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500;
+       @apply block py-2 px-3 text-white bg-ctp-sapphire rounded md:bg-transparent md:text-ctp-sapphire md:p-0 dark:text-white md:dark:text-ctp-sapphire;
     }
    
     .not-current-page {
-       @apply block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent;
+       @apply block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-ctp-sapphire md:p-0 dark:text-white md:dark:hover:text-ctp-sapphire dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent;
     }
    </style>
