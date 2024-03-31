e5da7bb417261c3d77c2cb0a7347ff9eab31c17d
Boofdev <hexahigh0@gmail.com>
2024-03-31 21:42:55 +0200

    lisens namnam

--- src/lib/components/footer.svelte
+++ src/lib/components/footer.svelte
@@ -18,6 +18,7 @@
             </div>
             <p>Versjon {getLatestVersion().id}.{commitHash} - {getLatestVersion().name}</p>
             <p>{new Date().getFullYear()} © Blålange Festivalen. All rights reserved.</p>
+            <p>The content of this project itself is licensed under the <a class="hover:underline" href="https://creativecommons.org/licenses/by/3.0/">Creative Commons Attribution 4.0 International license</a>, and the underlying source code used to format and display that content is licensed under the <a class="hover:underline" href="https://mozilla.org/MPL/2.0/">Mozilla Public License v2.0</a>.</p>
         </div>
     </div>
 </footer>
