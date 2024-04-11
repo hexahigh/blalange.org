a2febf0300c0621557b570aa0b23d20c812a8942
Boofdev <hexahigh0@gmail.com>
2024-04-11 09:08:07 +0200

    cleanup css

--- src/lib/components/countdown.svelte
+++ src/lib/components/countdown.svelte
@@ -72,30 +72,10 @@
         padding: 0;
     }
 
-    html,
-    body {
-        height: 100%;
-        margin: 0;
-    }
-
-    body {
-        align-items: center;
-        background-color: #ffffff;
-        display: flex;
-        font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto,
-            Oxygen-Sans, Ubuntu, Cantarell, "Helvetica Neue", sans-serif;
-    }
-
     .container {
         color: #000000;
         margin: 0 auto;
         text-align: center;
-    }
-
-    h1 {
-        font-weight: normal;
-        letter-spacing: 0.125rem;
-        text-transform: uppercase;
     }
 
     li {
@@ -119,19 +99,5 @@
     .emoji span {
         font-size: 4rem;
         padding: 0 0.5rem;
-    }
-
-    @media all and (max-width: 768px) {
-        h1 {
-            font-size: calc(1.5rem * var(--smaller));
-        }
-
-        li {
-            font-size: calc(1.125rem * var(--smaller));
-        }
-
-        li span {
-            font-size: calc(3.375rem * var(--smaller));
-        }
     }
 </style>
--- src/routes/sitemap.xml/+server.js
+++ src/routes/sitemap.xml/+server.js
@@ -1,3 +1,5 @@
+export const prerender = true;
+
 const pages = [
   "rebrand",
   "countdown",
