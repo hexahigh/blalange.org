4dd2cbc6fada7edf3abe98007f1bc1b147352400
Boofdev <hexahigh0@gmail.com>
2024-04-14 15:23:17 +0200

    maybe this will fix it?

--- src/lib/js/egg2.js
+++ src/lib/js/egg2.js
@@ -1,5 +1,25 @@
 import { Howl, Howler } from "howler";
-const voiceLines = import.meta.glob("$lib/audio/egg2/*.wav");
+
+import no1 from "$lib/audio/egg2/no1.wav";
+import no2 from "$lib/audio/egg2/no2.wav";
+import no3 from "$lib/audio/egg2/no3.wav";
+import generic1 from "$lib/audio/egg2/generic1.wav";
+import generic2 from "$lib/audio/egg2/generic2.wav";
+import generic3 from "$lib/audio/egg2/generic3.wav";
+import idiot from "$lib/audio/egg2/idiot.wav";
+import fess from "$lib/audio/egg2/fess.wav";
+import stupid from "$lib/audio/egg2/egg2/stupid.wav";
+import ugly from "$lib/audio/egg2/ugly.wav";
+import ass from "$lib/audio/egg2/ass.wav";
+import engiIncoming from "$lib/audio/egg2/engi-incoming.wav";
+import danke from "$lib/audio/egg2/danke.wav";
+import engiPray from "$lib/audio/egg2/engi-pray.wav";
+import engiDone from "$lib/audio/egg2/engi-done.wav";
+import engiSeen from "$lib/audio/egg2/engi-seen.wav";
+import engiBooo from "$lib/audio/egg2/engi-booo.wav";
+import engiMad1 from "$lib/audio/egg2/engi-mad1.wav";
+import engiMad2 from "$lib/audio/egg2/engi-mad2.wav";
+import engiDick from "$lib/audio/egg2/engi-dick.wav";
 
 let triggerCount = 0;
 
@@ -32,99 +52,93 @@ function trigger() {
 
   switch (triggerCount) {
     case 1:
-      play("no1.wav");
+      play(no1);
       break;
 
     case 2:
-      play("no2.wav");
+      play(no2);
       break;
 
     case 3:
-      play("no3.wav");
+      play(no3);
       break;
 
     case 4:
-      play("generic2.wav");
+      play(generic2);
       break;
 
     case 5:
-      play("generic1.wav");
+      play(generic1);
       break;
 
     case 6:
-      play("idiot.wav");
+      play(idiot);
       break;
 
     case 7:
-      play("fess.wav");
+      play(fess);
       break;
 
     case 8:
-      play("stupid.wav");
+      play(stupid);
       break;
 
     case 9:
-      play("generic3.wav");
+      play(generic3);
       break;
 
     case 10:
-      play("ugly.wav");
+      play(ugly);
       break;
 
     case 11:
-      play("ass.wav");
+      play(ass);
       break;
 
     case 12:
-      play("engi-incoming.wav");
+      play(engiIncoming);
       break;
 
     case 13:
-      play("danke.wav");
+      play(danke);
       break;
 
     case 14:
-      play("engi-pray.wav");
+      play(engiPray);
       break;
 
     case 15:
-      play("engi-done.wav");
+      play(engiDone);
       break;
 
     case 16:
-      play("engi-seen.wav");
+      play(engiSeen);
       break;
 
     case 17:
-      play("engi-booo.wav");
+      play(engiBooo);
       break;
 
     case 18:
-      play("engi-mad1.wav");
+      play(engiMad1);
       break;
 
     case 19:
-      play("engi-mad2.wav");
+      play(engiMad2);
       break;
 
     case 20:
-      play("engi-dick.wav");
+      play(engiDick);
       // Remove the entire page
       document.querySelector("html").remove();
       break;
   }
 }
 
-function play(filename) {
-  for (const key in voiceLines) {
-    if (key.endsWith(filename)) {
-      let sound = new Howl({
-        src: [key],
-      });
+function play(file) {
+  let sound = new Howl({
+    src: [file],
+  });
 
-      sound.play();
-
-      break;
-    }
-  }
+  sound.play();
 }
