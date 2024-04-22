cacfa03db39c6c25badb04a5cc47e22492f8a96a
Boofdev <hexahigh0@gmail.com>
2024-04-22 11:42:00 +0200

    fix

--- src/hooks.server.js
+++ src/hooks.server.js
@@ -1,11 +1,11 @@
 /** @type {import('@sveltejs/kit').Handle} */
-export async function handle({ request, resolve }) {
+export async function handle({ event, resolve }) {
   // If domain is kukfest.eu then redirect to blalange.org
-  if (request.url.hostname === "kukfest.eu") {
-      return Response.redirect("https://blalange.org" + request.url.pathname, 301);
+  if (event.url.hostname === "kukfest.eu") {
+      return Response.redirect("https://blalange.org" + event.url.pathname, 301);
   }
 
-  const response = await resolve(request);
+  const response = await resolve(event);
 
   // Allow CORS requests from everywhere
   response.headers.set('Access-Control-Allow-Origin', '*');
