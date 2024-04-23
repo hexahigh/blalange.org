acd8dbace097fa03fbe3956848278883b80217fd
Boofdev <hexahigh0@gmail.com>
2024-04-23 08:31:10 +0200

    fix httping

--- src/routes/term/commands/httping.js
+++ src/routes/term/commands/httping.js
@@ -22,7 +22,7 @@ export async function main(print,  inOptions = {}) {
     // Get current unix time
     let now = Date.now();
 
-    if (!options.url.startsWith("http://") || !options.url.startsWith("https://")) {
+    if (!options.url.startsWith("http://") && !options.url.startsWith("https://")) {
         // Add http prefix
         options.url = "http://" + options.url;
     }
