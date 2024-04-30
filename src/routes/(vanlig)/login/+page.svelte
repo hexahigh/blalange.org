3b8bbd55105c2748d03f1b8fe3e1d97067e98674
Boofdev <hexahigh0@gmail.com>
2024-04-30 16:32:22 +0200

    logins now work in the comment section

--- src/routes/(vanlig)/login/+page.svelte
+++ src/routes/(vanlig)/login/+page.svelte
@@ -26,19 +26,19 @@
 </script>
 
 <div class="flex flex-col items-center justify-center min-h-screen">
-  <div class="p-6 mt-10 rounded shadow-md shadow-black w-80 text-center">
+  <div class="p-6 mt-10 rounded shadow-md shadow-black w-80 text-center dark:text-white">
     <h1 class="text-2xl font-bold mb-4">Login</h1>
     <form on:submit|preventDefault>
       <input
         placeholder="Username"
         bind:value={user}
-        class="w-full mb-4 p-2 border-black border-2 rounded"
+        class="w-full mb-4 p-2 border-black border-2 rounded dark:bg-gray-900 dark:border-gray-700"
       />
       <input
         placeholder="Password"
         type="password"
         bind:value={pass}
-        class="w-full mb-4 p-2 border-black border-2 rounded"
+        class="w-full mb-4 p-2 border-black border-2 rounded dark:bg-gray-900 dark:border-gray-700"
       />
       <button
         on:click={auth}
