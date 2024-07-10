e52aeea5688fc5ddd483982483485469aac60793
Boofdev <hexahigh0@gmail.com>
2024-07-10 17:27:29 +0200

    articles support markdown

--- src/routes/(vanlig)/a/[slug]/+page.ts
+++ src/routes/(vanlig)/a/[slug]/+page.ts
@@ -1,5 +1,6 @@
 import PocketBase from "pocketbase";
 import { error } from "@sveltejs/kit";
+import { marked } from "marked";
 
 export async function load({ params, url }) {
   try {
@@ -19,6 +20,9 @@ export async function load({ params, url }) {
     const imgUrl = await pb.files.getUrl(article, article.image);
 
     article.date = new Date(article.date).getTime();
+
+    // Process the markdown
+    article.text = marked.parse(article.text);
 
     // Fetch the author names
     const authors = await Promise.all(
