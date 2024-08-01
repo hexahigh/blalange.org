ef4cb3b7c61ddd7e5fb50e4bcfb4f54c182b14e0
Boofdev <hexahigh0@gmail.com>
2024-08-01 21:17:59 +0200

    added tos

---
+++ src/routes/(vanlig)/tos/+page.svelte
@@ -0,0 +1,252 @@
+<script lang="ts">
+  import { config, defaultConfig } from "$lib/js/config";
+  import Metatags from "$lib/components/metatags.svelte";
+</script>
+
+<Metatags
+  title="TOS"
+  description="TOS for blalange.org"
+  url="/tos"
+/>
+
+<div class="max-w-4xl mx-auto px-4 sm:px-6 lg:px-8 py-12">
+  <h1>ACCEPTANCE OF TERMS</h1>
+  <p>
+    Blålange Festivalen provides the information on this Web site as provided in these Terms of Service (“TOS”).
+    Blålange Festivalen may update the TOS at any time and without prior notice by posting a new version at https://{defaultConfig.primaryDomain}/tos.
+    The information on this site and your use of it is subject to the most recent version of the TOS posted.
+  </p>
+
+  <h2>Description of Service</h2>
+  <p>
+    Blålange Festivalen provides you with access to chatrooms, articles and a variety of other services. ("Blålange
+    Festivalen Service"). Unless explicitly stated otherwise, any new features that augment or enhance the current
+    Blålange Festivalen Service shall be subject to the TOS.
+  </p>
+
+  <h2>YOUR REGISTRATION OBLIGATIONS</h2>
+  <p>
+    In consideration of your use of the Blålange Festivalen Service, you represent that you are of legal age to form a
+    binding contract and are not a person barred from receiving services under the laws of the United States or other
+    applicable jurisdiction. You also agree to:
+  </p>
+  <ul class="list-disc pl-8">
+    <li>
+      provide true, accurate, current and complete information about yourself as prompted by Blålange Festivalen's
+      registration form (the “Registration Data”) and
+    </li>
+    <li>
+      maintain and promptly update the Registration Data to keep it true, accurate, current and complete. If you provide
+      any information that is untrue, inaccurate, not current or incomplete, or Blålange Festivalen has reasonable
+      grounds to suspect that such information is untrue, inaccurate, not current or incomplete, Blålange Festivalen has
+      the right to suspend or terminate your account and refuse any and all current or future use of the Blålange
+      Festivalen Services (or any portion thereof).
+    </li>
+  </ul>
+
+  <h2>PRIVACY POLICY</h2>
+  <p>
+    Registration Data and certain other information about you is subject to our <a href="/privacy">Privacy Policy</a>
+  </p>
+
+  <h2>CONDUCT</h2>
+  <p>
+    You understand that all information, data, text, software, graphics or other materials (“Content”), whether publicly
+    posted or privately transmitted, are the sole responsibility of the person from whom such Content originated. This
+    means that you, and not Blålange Festivalen, are entirely responsible for all Content that you upload, post, email,
+    transmit or otherwise make available via the Blålange Festivalen Service. Blålange Festivalen does not control the
+    Content posted via the Blålange Festivalen Service and, as such, does not guarantee the accuracy, integrity or
+    quality of such Content. Under no circumstances will Blålange Festivalen be liable in any way for any Content,
+    including, but not limited to, any errors or omissions in any Content, or any loss or damage of any kind incurred as
+    a result of the use of any Content posted, emailed, transmitted or otherwise made available via the Blålange
+    Festivalen Service. You agree to not use the Blålange Festivalen Service to:
+  </p>
+  <ol class="pl-8">
+    <li>
+      upload, post, email, transmit or otherwise make available any Content that is unlawful, harmful, threatening,
+      abusive, harassing, tortuous, defamatory, vulgar, obscene, libelous, invasive of another's privacy, hateful, or
+      racially, ethnically or otherwise objectionable or harm minors;
+    </li>
+    <li>
+      impersonate any person or entity or falsely state or otherwise misrepresent your affiliation with a person or
+      entity;
+    </li>
+    <li>
+      forge headers or otherwise manipulate identifiers in order to disguise the origin of any Content transmitted
+      through the Blålange Festivalen Service;
+    </li>
+    <li>
+      upload, post, email, transmit or otherwise make available any Content that you do not have a right to make
+      available under any law or under contractual or other relationships;
+    </li>
+    <li>
+      upload, post, email, transmit or otherwise make available any Content that infringes any patent, trademark, trade
+      secret, copyright or other proprietary rights of any party;
+    </li>
+    <li>
+      upload, post, email, transmit or otherwise make available any unsolicited or unauthorized advertising, promotional
+      materials, “junk mail,” “spam,” or any other form of solicitation;
+    </li>
+    <li>
+      upload, post, email, transmit or otherwise make available any material that contains software viruses or any other
+      computer code, files or programs designed to interrupt, destroy or limit the functionality of any computer
+      software or hardware or telecommunications equipment;
+    </li>
+    <li>
+      disrupt the normal flow of dialogue, or otherwise act in a manner that negatively affects other users' ability to
+      engage in discussions or exchanges;
+    </li>
+    <li>
+      interfere with or disrupt the Blålange Festivalen Service or servers or networks connected to the Blålange
+      Festivalen Service, or disobey any requirements, procedures, policies or regulations of networks connected to the
+      Blålange Festivalen Service;
+    </li>
+    <li>intentionally or unintentionally violate any applicable local, state, national or international law,</li>
+    <li>“stalk” or otherwise harass another; and/or</li>
+    <li>
+      collect or store personal data about other users in connection with the prohibited conduct and activities set
+      forth in paragraphs above. You acknowledge that Blålange Festivalen may or may not pre-screen Content, but that
+      Blålange Festivalen and its designees shall have the right (but not the obligation) in their sole discretion to
+      pre-screen, refuse, move, or remove any Content that is available via the Blålange Festivalen Service and which
+      violates the TOS. You agree that you must evaluate, and bear all risks associated with, the use of any Content,
+      including any reliance on the accuracy, completeness, or usefulness of such Content. You acknowledge, consent and
+      agree that Blålange Festivalen may access, preserve and disclose your account information and Content if required
+      to do so by law or in a good faith belief that such access preservation or disclosure is reasonably necessary to:
+    </li>
+    <ul class="pl-8 list-disc">
+      <li>comply with legal process;</li>
+      <li>enforce the TOS;</li>
+      <li>respond to claims that any Content violates the rights of third parties;</li>
+      <li>respond to your requests for customer service; or</li>
+      <li>protect the rights, property or personal safety of Blålange Festivalen, its users and the public.</li>
+    </ul>
+  </ol>
+
+  <h2>CONTENT MADE AVAILABLE FOR INCLUSION ON THE BLÅLANGE FESTIVALEN SERVICE</h2>
+  <p>
+    Blålange Festivalen does not claim ownership of Content you submit or make available for inclusion via the Blålange Festivalen Service. However,
+    with respect to Content you submit or make available for inclusion on the publicly available Blålange Festivalen Service, you
+    irrevocably grant Blålange Festivalen the perpetual, worldwide, royalty-free and non-exclusive license, with the right to sublicense
+    through multiple tiers of sublicensees, to use, distribute, reproduce, modify, adapt, publicly perform and publicly
+    display such Content, in whole or in part, on the Blålange Festivalen Service or other publications by Blålange Festivalen in any media whether now
+    existing or which come into the existence into the future and to provide that information under the license set
+    forth on the initial page of the http://www.opensource.org/ Web site. And for Content you submit for private
+    discussions, you grant Blålange Festivalen the worldwide, royalty-free and non-exclusive license to use, distribute, reproduce,
+    modify, adapt, publicly perform and publicly display such Content on the associated private discussion group.
+  </p>
+
+  <h2>INDEMINITY</h2>
+  <p>
+    You agree to indemnify and hold Blålange Festivalen and its subsidiaries, affiliates, officers, agents, employees, partners and
+    licensors harmless from any claim or demand, including but not limited to reasonable attorneys' fees, made by any
+    third party due to or arising out of Content you submit, post, transmit or otherwise make available through the
+    Service, your use of the Blålange Festivalen Service, your connection to the Service, your violation of the TOS, or your violation
+    of any rights of another.
+  </p>
+
+  <h2>MODIFICATIONS TO Blålange Festivalen SERVICE</h2>
+  <p>
+    You acknowledge that Blålange Festivalen may establish general practices and limits concerning use of the Blålange Festivalen Service, including
+    without limitation the maximum number of days that email messages, message board postings or other uploaded Content
+    will be retained by the Blålange Festivalen Service.. You further acknowledge that Blålange Festivalen reserves the right to modify these general
+    practices and limits from time to time. Blålange Festivalen reserves the right at any time and from time to time to modify or
+    discontinue, temporarily or permanently, the Blålange Festivalen Service (or any part thereof) with or without notice. You agree
+    that Blålange Festivalen shall not be liable to you or to any third party for any modification, suspension or discontinuance of the
+    Blålange Festivalen Service.
+  </p>
+
+  <h2>TERMINATION</h2>
+  <p>
+    You agree that Blålange Festivalen may terminate your access to the Blålange Festivalen Service for violations of the TOS and/or requests by
+    authorized law enforcement or other government agencies.
+  </p>
+
+  <h2>LINKS</h2>
+  <p>
+    The Blålange Festivalen Service may provide, or third parties may provide, links to other World Wide Web sites or resources. Because
+    Blålange Festivalen has no control over such sites and resources, you acknowledge and agree that Blålange Festivalen is not responsible for the
+    availability of such external sites or resources, and does not endorse and is not responsible or liable for any
+    Content, advertising, products or other materials on or available from such sites or resources. You further
+    acknowledge and agree that Blålange Festivalen shall not be responsible or liable, directly or indirectly, for any damage or loss
+    caused or alleged to be caused by or in connection with use of or reliance on any such Content, goods or services
+    available on or through any such site or resource.
+  </p>
+
+  <h2>DISCLAIMER OF WARRANTIES</h2>
+  <p>
+    You expressly understand and agree that: Your use of the Blålange Festivalen service is at your sole risk. Blålange Festivalen service is provided
+    on an “as is” and “as available” basis. Blålange Festivalen and its subsidiaries, affiliates, officers, employees and licensors
+    expressly disclaim all warranties of any kind, whether express or implied, including, but not limited to the implied
+    warranties of merchantability, fitness for a particular purpose and non-infringement. Blålange Festivalen and its subsidiaries,
+    affiliates, officers, employees and licensors make no warranty that
+  </p>
+  <ol>
+    <li>The Blålange Festivalen service will meet your requirements;</li>
+    <li>The Blålange Festivalen service will be uninterrupted, timely, secure or error-free and</li>
+    <li>
+      The results that may be obtained from the use of the Blålange Festivalen service will be accurate or reliable. No advice or
+      information, whether oral or written, obtained by you from Blålange Festivalen or through or from the service shall create any
+      warranty not expressly stated in the TOS.
+    </li>
+  </ol>
+
+  <h2>LIMITATION OF LIABILITY</h2>
+  <p>
+    You expressly understand and agree that Blålange Festivalen and its subsidiaries, affiliates, officers, employees, agents, partners
+    and licensors shall not be liable to you for any direct, indirect, incidental, special, consequential or exemplary
+    damages, including, but not limited to, damages for loss of profits, goodwill, use, data or other intangible losses
+    (even if Blålange Festivalen has been advised of the possibility of such damages), resulting from the use or the inability to use
+    the Blålange Festivalen service.
+  </p>
+
+  <h2>NOTICE AND PROCEDURE FOR MAKING CLAIMS OF COPYRIGHT INFRINGEMENT</h2>
+  <p>
+    Blålange Festivalen respects the copyright of others, and we ask our users to do the same. Blålange Festivalen may, in appropriate circumstances and
+    at its discretion, disable and/or terminate the access of users who may be repeat infringers. If you believe that
+    your work has been copied in a way that constitutes copyright infringement, please provide Blålange Festivalen's Copyright Agent the
+    following information: an electronic or physical signature of the person authorized to act on behalf of the owner of
+    the copyright; a description of the copyrighted work that you claim has been infringed; a description of where the
+    material that you claim is infringing is located on the site; your address, telephone number, and email address; a
+    statement by you that you have a good faith belief that the disputed use is not authorized by the copyright owner,
+    its agent, or the law; a statement by you, made under penalty of perjury, that the above information in your Notice
+    is accurate and that you are the copyright owner or authorized to act on the copyright owner's behalf. Blålange Festivalen's Agent
+    for Notice of claims of copyright infringement can be reached as follows:
+    <br />
+    <br />
+    By email:
+    <br />
+    <a href="mailto:copr@blalange.org">copr@blalange.org</a>
+  </p>
+
+  <h2>GENERAL INFORMATION</h2>
+  <p>
+    Entire Agreement. The TOS constitutes the entire agreement between you and Blålange Festivalen and governs your use of the Blålange Festivalen
+    Service, superseding any prior agreements between you and Blålange Festivalen with respect to the Blålange Festivalen Service.. Choice of Law and
+    Forum. The TOS and the relationship between you and Blålange Festivalen shall be governed by the laws of the State of California
+    without regard to its conflict of law provisions. You and Blålange Festivalen agree to submit to the personal and exclusive
+    jurisdiction of the courts located within the county of Santa Clara, California. Waiver and Severability of Terms.
+    The failure of Blålange Festivalen to exercise or enforce any right or provision of the TOS shall not constitute a waiver of such
+    right or provision. If any provision of the TOS is found by a court of competent jurisdiction to be invalid, the
+    parties nevertheless agree that the court should endeavor to give effect to the parties' intentions as reflected in
+    the provision, and the other provisions of the TOS remain in full force and effect.
+  </p>
+</div>
+
+<style lang="postcss">
+  h1 {
+    @apply text-3xl font-bold text-center mb-8;
+  }
+  h2 {
+    @apply text-2xl font-semibold mt-8 mb-4;
+  }
+  a {
+    @apply underline text-primary-400;
+  }
+  li {
+    @apply my-2;
+  }
+  ol {
+    list-style-type: decimal;
+    @apply pl-8;
+  }
+</style>
