230664f47945651a05bd8d0150dfbcf23cbc6849
Boofdev <hexahigh0@gmail.com>
2024-10-07 15:58:18 +0200

    ✨ feat(article list): Added search
    Fancy schmancy search

---
+++ src/lib/components/search.svelte
@@ -0,0 +1,43 @@
+<script lang="ts">
+  export let placeholder: string = "Søk...";
+  export let onSubmit: ((event: SubmitEvent) => void)
+</script>
+
+<form class="flex items-center max-w-sm mx-auto" on:submit={onSubmit}>
+  <label for="simple-search" class="sr-only">Søk</label>
+  <div class="relative w-full">
+    <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+      <svg class="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+        <path
+          stroke="currentColor"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="2"
+          d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
+        />
+      </svg>
+    </div>
+    <input
+      type="text"
+      id="simple-search"
+      class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5 dark:bg-gray-800 dark:border-gray-700 dark:placeholder-gray-500 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+      {placeholder}
+      required
+    />
+  </div>
+  <button
+    type="submit"
+    class="p-2.5 ms-2 text-sm font-medium text-white bg-blue-700 rounded-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+  >
+    <svg class="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+      <path
+        stroke="currentColor"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        stroke-width="2"
+        d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
+      />
+    </svg>
+    <span class="sr-only">Søk</span>
+  </button>
+</form>
