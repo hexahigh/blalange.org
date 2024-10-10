145aff2ac3a11ecca3d28fcbef2006a53e79af93
Boofdev <hexahigh0@gmail.com>
2024-10-10 17:42:53 +0200

    refactor(session): Renamed getPermanentId to getPersistentId

---
+++ src/lib/js/session.ts
@@ -0,0 +1,54 @@
+/**
+ * Gets the session ID from session storage. If no session ID exists, generates
+ * a new one and stores it.
+ *
+ * @returns {string} The session ID.
+ */
+export function getSessionId(): string {  
+    // Check if a session ID already exists in local storage
+    let sessionId = sessionStorage.getItem('sessionId');
+
+    // If no session ID exists, generate a new one and store it
+    if (!sessionId) {
+        console.log("No session ID found. Generating a new one.");
+        sessionId = generateUniqueId();
+        sessionStorage.setItem('sessionId', sessionId);
+    } else {
+        console.log("Existing session ID found");
+    }
+
+    return sessionId;
+}
+
+/**
+ * Retrieves a persistent ID from local storage. If no ID exists, a new one is
+ * generated and stored. This ID is used to identify the user across sessions.
+ * @returns {string} The persistent ID.
+ */
+export function getPersistentId(): string {  
+    // Check if a persistent ID already exists in local storage
+    let persistentId = localStorage.getItem('persistentId');
+    
+    // If no persistent ID exists, generate a new one and store it
+    if (!persistentId) {
+        console.log("No persistent ID found. Generating a new one.");
+        persistentId = generateUniqueId();
+        localStorage.setItem('persistentId', persistentId);
+    } else {
+        console.log("Existing persistent ID found");
+    }
+
+    return persistentId;
+}
+
+/**
+ * @deprecated Use {@link getPersistentId} instead.
+ */
+export function getPermanentId() {
+    return getPersistentId();
+}
+
+function generateUniqueId() {
+    // Generate a unique ID using a combination of Date.now() and Math.random()
+    return 'session-' + Date.now() + '-' + Math.random().toString(36).substr(2, 9);
+}
