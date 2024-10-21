0ac340852374260b28cd97e9e4cf5115fa027b3b
Boofdev <hexahigh0@gmail.com>
2024-10-21 22:21:19 +0200

    style(article): Remove obsolete comment

--- src/routes/(vanlig)/a/[slug]/+page.svelte
+++ src/routes/(vanlig)/a/[slug]/+page.svelte
@@ -34,7 +34,6 @@
   <div>
     <!-- svelte-ignore a11y-img-redundant-alt -->
     {#if image}
-    <!-- style="background: radial-gradient(ellipse at center, {averageColor} 0%, transparent 175%);" -->
       <img src={image} class="w-screen lg:aspect-[16/9] object-contain rounded-3xl" alt="Article Image" id="articleImg" />
     {/if}
     <h1 class="text-2xl font-medium">
