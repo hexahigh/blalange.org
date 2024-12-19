ddeef8d65673976d04b2fb04382159d9acf480d2
t0m0t0w <57962470+t0m0t0w@users.noreply.github.com>
2024-12-19 07:56:14 +0000

    feat: Add 3cx widget
    jeg lærte meg :)
    
    simon

--- src/routes/(vanlig)/+layout.svelte
+++ src/routes/(vanlig)/+layout.svelte
@@ -1,6 +1,7 @@
 <script>
   import Footer from "$lib/components/footer.svelte";
   import Nav from "$lib/components/nav.svelte";
+  import Support from "$lib/components/3cxWidget.svelte";
   /**
    * @typedef {Object} Props
    * @property {import('svelte').Snippet} [children]
@@ -16,6 +17,6 @@
   <main class="flex-grow dark:bg-gray-900 text-black dark:text-white">
     {@render children?.()}
   </main>
-
+  <Support />
   <Footer />
 </div>
